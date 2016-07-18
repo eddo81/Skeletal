cec93658520b1c1ca9aefb165a9e1c0aee7021f9
Eduardo Jönnerstig <eduardo.jonnerstig@fondmarknaden.se>
2016-07-18 23:13:03 +0200

    updated default gulp-task

--- src/tools/gulp/default-task.js
+++ src/tools/gulp/default-task.js
@@ -9,14 +9,21 @@ const runSequence = require('run-sequence');
 
 module.exports = function (gulp, plugins, config) {
 
-  let sequence = ['lint', 'html', 'js', 'images', 'fonts', 'copy'];
-
-  if(config.debug) {
-    sequence.push('watch');
-  }
-
   gulp.task('default', ['clean'], function (callback) {
-    runSequence(['scss'], sequence, callback);
+
+    let preSequence = ['scss'];
+    let sequence = ['lint', 'html', 'js', 'images', 'fonts', 'copy'];
+    let postSequence = ['watch'];
+
+    let args = [preSequence, sequence, postSequence, callback];
+
+    // Remove the watch-task from the args array if in "Production mode".
+    if(!config.debug) {
+      args.splice(2, 1);
+    }
+
+    // Apply the args array as a comma separated list of arguments to runSequence.
+    runSequence.apply(undefined, args);
   });
 
 };
