01f264f31c1d20e7de3d4e0110d44ddddb258d0a
eddo81 <eduardo_jonnerstig@live.com>
2017-06-18 12:47:58 +0200

    updated eslint-task, package.json

--- src/tools/gulp/lint-task.js
+++ src/tools/gulp/lint-task.js
@@ -11,7 +11,8 @@ module.exports = (gulp, plugins, config) => {
     gulp.src([config.globs.root_src + config.globs.folders.src.scripts + '**/*.js', config.globs.root_src + config.globs.folders.src.framework + '**/*.js'])
       .pipe(plugins.eslint())
       .pipe(plugins.eslint.format())
-      .pipe(!plugins.browserSync.active ? plugins.eslint.failOnError() : plugins.util.noop());
-      callback();
+      .pipe(!plugins.browserSync.active ? plugins.eslint.failAfterError() : plugins.util.noop())
+      .on('error', (err) => { /*plugins.util.log(err);*/ });
+      callback(); 
   });
 };
